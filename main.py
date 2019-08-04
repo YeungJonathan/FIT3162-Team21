76d8d6b060907cdc34c3b1cd9b5afe30eab4fb93
Gabby <salufie@gmail.com>
2019-08-04 21:07:25 +1000

    changed to pd.read_excel

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 
 #read excel file
-test = pd.read_excel (r'VBA_data.xls')
+test = pd.read_excel('VBA_data.xls')
 
 #print first 5 rows of excel
 print(test.head())
