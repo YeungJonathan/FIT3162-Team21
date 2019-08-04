2bc6e85b9a9ffa5d5f7e299ba1fe2ffb93dc68af
Jonathan Yeung <jonathanyeung1997@gmail.com>
2019-08-04 22:19:12 +1000

    First try on decision tree implementation

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
+# Before running:
+```
+brew install graphviz
+pip install pandas
+pip install sklearn
+```
+
 # POTENTIAL ERRORS
 Do you have the following errors when running our project?  
 Error 1: ImportError: Install xlrd >= 0.9.0 for Excel support  
--- main.py
+++ main.py
@@ -1,8 +1,47 @@
 import pandas as pd
 import numpy as np
+import graphviz 
+from sklearn import datasets, tree
 
 #read excel file
-test = pd.read_excel('VBA_data.xls')
+vba = pd.read_excel('VBA_data.xls')
 
-#print first 5 rows of excel
-print(test.head())
+ufi = vba['UFI']
+taxon_ids = vba['TAXON_ID']
+scientific_display = vba['SCIENTIFIC_DISPLAY_NME']
+reliability = vba['RELIABILITY']
+latitude = vba["LATITUDEDD_NUM"]
+longitude = vba["LONGITUDEDD_NUM"]
+
+# unique_taxon {key:taxon_id, data: [target_id, scientific_display]}
+unique_taxon = {}
+data_list = []
+target_list = []
+current_target_id = 0    
+for index in range(len(ufi)):
+    try:
+        target_id = unique_taxon[taxon_ids[index]][0]
+    except:
+        unique_taxon[taxon_ids[index]] = [current_target_id, scientific_display[index]]
+        target_id = current_target_id
+        current_target_id += 1
+
+    if reliability[index] == "Acceptable":
+        current_reliability = 0
+    elif reliability[index] == "Confirmed":
+        current_reliability = 1
+    elif reliability[index] == "Unconfirmed":
+        current_reliability = 2
+    else:
+        current_reliability = 3
+        
+    data_list.append([current_reliability, latitude[index], longitude[index]])
+    target_list.append(target_id)
+
+
+clf = tree.DecisionTreeClassifier()
+clf = clf.fit(data_list, target_list)
+tree.plot_tree(clf.fit(data_list, target_list)) 
+dot_data = tree.export_graphviz(clf, out_file=None) 
+graph = graphviz.Source(dot_data) 
+graph.render("vba")
