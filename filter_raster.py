21c179868358d832e4002f989585af094541294f
Jonathan Yeung <jonathanyeung1997@gmail.com>
2019-08-11 17:21:56 +1000

    Added filter raster file which returns matrix and geolocation of data

--- filter_raster.py
+++ filter_raster.py
@@ -1,4 +1,8 @@
 from osgeo import gdal
+import numpy as np
+import rasterio
+import rasterio.features
+import rasterio.warp
 
 dataset = gdal.Open('vegtype3_4/vegtype3_4', gdal.GA_ReadOnly)
 if not dataset:
@@ -6,8 +10,6 @@ if not dataset:
 
 # general information of the raster datasets
 print()
-print("Driver: {}/{}".format(dataset.GetDriver().ShortName,
-                            dataset.GetDriver().LongName))
 print("Size is {} x {} x {}".format(dataset.RasterXSize,
                                     dataset.RasterYSize,
                                     dataset.RasterCount))
@@ -18,3 +20,29 @@ if geotransform:
     print("Pixel Size = ({}, {})".format(geotransform[1], geotransform[5]))
 
 print()
+
+# get raster data into numpy array
+myarray = np.array(dataset.GetRasterBand(1).ReadAsArray())
+print('raster data matrix: ',myarray,'\n')
+
+# print geolocation of datas
+with rasterio.open('vegtype3_4/vegtype3_4') as dataset:
+    # Read the dataset's valid data mask as a ndarray.
+    mask = dataset.dataset_mask()
+
+    # Extract feature shapes and values from the array.
+    for geom, val in rasterio.features.shapes(
+            mask, transform=dataset.transform):
+
+        # Transform shapes from the dataset's own coordinate
+        # reference system to CRS84 (EPSG:4326).
+        geom = rasterio.warp.transform_geom(
+            dataset.crs, 'EPSG:4326', geom, precision=6)
+
+        # Print GeoJSON shapes to stdout.
+        print('geometric locations when raster data of type: ',geom , '\n')
+
+    band = dataset.read(1)
+    print('raster data four corner boundary: ',dataset.bounds, '\n')
+    print('location top left: ', dataset.transform * (0,0), '\n')
+    print('location bottom right: ',dataset.transform * (dataset.height, dataset.width), '\n')
