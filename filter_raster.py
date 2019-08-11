fb4941b7b6d26ddfe14cfed79961145b048028c7
Jonathan Yeung <jonathanyeung1997@gmail.com>
2019-08-11 16:08:59 +1000

    Can now get raster general informaion

---
+++ filter_raster.py
@@ -0,0 +1,20 @@
+from osgeo import gdal
+
+dataset = gdal.Open('vegtype3_4/vegtype3_4', gdal.GA_ReadOnly)
+if not dataset:
+    print("error opening dataset")
+
+# general information of the raster datasets
+print()
+print("Driver: {}/{}".format(dataset.GetDriver().ShortName,
+                            dataset.GetDriver().LongName))
+print("Size is {} x {} x {}".format(dataset.RasterXSize,
+                                    dataset.RasterYSize,
+                                    dataset.RasterCount))
+print("Projection is {}".format(dataset.GetProjection()))
+geotransform = dataset.GetGeoTransform()
+if geotransform:
+    print("Origin = ({}, {})".format(geotransform[0], geotransform[3]))
+    print("Pixel Size = ({}, {})".format(geotransform[1], geotransform[5]))
+
+print()
