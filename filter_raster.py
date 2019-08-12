b523f592e4f17aeba79760c5dc1108b338fd896e
Jonathan Yeung <jonathanyeung1997@gmail.com>
2019-08-12 12:40:50 +1000

    Added comment for good practices

--- filter_raster.py
+++ filter_raster.py
@@ -5,12 +5,23 @@ import rasterio.features
 import rasterio.warp
 
 def open_raster_file(filePath):
+    '''
+    Function used to open the raster_file
+    :params:
+        filePath: the path of the raster file
+    '''
     dataset = gdal.Open(filePath, gdal.GA_ReadOnly)
     if not dataset:
         raise Exception("Error opening dataset")
     return dataset
 
+
 def get_raster_general_information(filePath):
+    '''
+    Function that gets the general information of the raster file
+    :params:
+        filePath: the path of the raster file
+    '''
     dataset = open_raster_file(filePath)
 
     # general information of the raster datasets
@@ -23,10 +34,15 @@ def get_raster_general_information(filePath):
     if geotransform:
         print("Origin = ({}, {})".format(geotransform[0], geotransform[3]))
         print("Pixel Size = ({}, {})".format(geotransform[1], geotransform[5]))
-
     print()
+   
     
 def get_raster_data_matrix(filePath):
+    '''
+    Function that gets the raster file matrix values
+    :params:
+        filePath: the path of the raster file
+    '''
     # get raster data into numpy array
     dataset = open_raster_file(filePath)
 
@@ -34,7 +50,13 @@ def get_raster_data_matrix(filePath):
     print('raster data matrix: ',raster_array,'\n')
     return raster_array
 
+
 def get_raster_geolocation(filePath):
+    '''
+    function that finds the corner gelocations of the raster file
+    :params:
+        filePath: the path of the raster file
+    '''
     # print geolocation of datas
     with rasterio.open(filePath) as dataset:
         # Read the dataset's valid data mask as a ndarray.
@@ -65,7 +87,24 @@ def get_raster_geolocation(filePath):
         returnMap['bottom_right'] = dataset.transform * (dataset.height, dataset.width)
         return returnMap, dataset.height, dataset.width
 
+
 def get_location_information(data_height, data_width, four_corners, raster_array, latitude, longitude):
+    '''
+    Function that gets the value from the matrix for a particular lat and long
+    :params:
+        data_height: height of the matrix
+        data_width: width of the matrix
+        four_corners: map of the values for the four corners
+        raster_array: the matrix of raster data
+        latitude: the latitude that we are finding
+        longitude: the longitude that we are finding 
+    :return:
+        (err, Value)
+        err: whether an error occured based on the range of the lat and long
+        value: 
+            1. err is True, value is the err message
+            2. err is False, value is the corresponding value in the matrix
+    '''
     if latitude < four_corners['top_left'][0] or latitude > four_corners['top_right'][0]:
         return True, "Latitude out of range"
     elif longitude > four_corners['top_left'][1] or longitude < four_corners['bottom_left'][1]:
