391aff2ec5531d0493adfd3072aef23b00fb2653
Gabby <salufie@gmail.com>
2019-08-13 17:16:35 +1000

    Starting to export (long, lat, raster) ect to excel file for model

--- gabby_raster.py
+++ gabby_raster.py
@@ -7,6 +7,7 @@ import numpy as np
 import rasterio
 import rasterio.features
 import rasterio.warp
+import xlsxwriter
 
 def changeCoordinateSystem(point):
     inProj = Proj(init='epsg:4326')
@@ -24,39 +25,39 @@ def calculateRaster(point, yOri, xOri, type, pixw, pixh):
 # --------------------------------------------------------------------
 #      1. VEG
 # --------------------------------------------------------------------
-dataset = gdal.Open('../raster/vegtype3_4/vegtype3_4')
-band = dataset.GetRasterBand(1)
-
-cols = dataset.RasterXSize
-rows = dataset.RasterYSize
-
-t = dataset.GetGeoTransform()
-
-xOrigin = t[0]
-yOrigin = t[3]
-pixelWidth = t[1]
-pixelHeight = -t[5]
-
-veg = band.ReadAsArray(0, 0, cols, rows)
+# dataset = gdal.Open('../raster/vegtype3_4/vegtype3_4')
+# band = dataset.GetRasterBand(1)
+#
+# cols = dataset.RasterXSize
+# rows = dataset.RasterYSize
+#
+# t = dataset.GetGeoTransform()
+#
+# xOrigin = t[0]
+# yOrigin = t[3]
+# pixelWidth = t[1]
+# pixelHeight = -t[5]
+#
+# veg = band.ReadAsArray(0, 0, cols, rows)
 
 # --------------------------------------------------------------------
 #      2. WETNESS
 # --------------------------------------------------------------------
 
-dataset = gdal.Open('../raster/wetness_index_saga_sept2012/wetness_index_saga_sept2012')
-band = dataset.GetRasterBand(1)
-
-cols = dataset.RasterXSize
-rows = dataset.RasterYSize
-
-t = dataset.GetGeoTransform()
-
-xOrigin_wet = t[0]
-yOrigin_wet = t[3]
-pixelWidth_wet = t[1]
-pixelHeight_wet = -t[5]
-
-wet = band.ReadAsArray(0, 0, cols, rows)
+# dataset = gdal.Open('../raster/wetness_index_saga_sept2012/wetness_index_saga_sept2012')
+# band = dataset.GetRasterBand(1)
+#
+# cols = dataset.RasterXSize
+# rows = dataset.RasterYSize
+#
+# t = dataset.GetGeoTransform()
+#
+# xOrigin_wet = t[0]
+# yOrigin_wet = t[3]
+# pixelWidth_wet = t[1]
+# pixelHeight_wet = -t[5]
+#
+# wet = band.ReadAsArray(0, 0, cols, rows)
 
 # --------------------------------------------------------------------
 #      3. SUMMER 1
@@ -80,7 +81,20 @@ wet = band.ReadAsArray(0, 0, cols, rows)
 # --------------------------------------------------------------------
 #      4. SUMMER 2
 # --------------------------------------------------------------------
-
+# dataset = gdal.Open('../raster/SummerLandsat75_300_900m/SummerLandsat75_300_900m')
+# band = dataset.GetRasterBand(1)
+#
+# cols = dataset.RasterXSize
+# rows = dataset.RasterYSize
+#
+# t = dataset.GetGeoTransform()
+#
+# xOrigin_summer75 = t[0]
+# yOrigin_summer75 = t[3]
+# pixelWidth_summer75 = t[1]
+# pixelHeight_summer75 = -t[5]
+#
+# summer75 = band.ReadAsArray(0, 0, cols, rows)
 # --------------------------------------------------------------------
 #      5. RAINFALL JULY
 # --------------------------------------------------------------------
@@ -123,52 +137,249 @@ wet = band.ReadAsArray(0, 0, cols, rows)
 #      7. MINTEMP JUL
 # --------------------------------------------------------------------
 
+# dataset = gdal.Open('../raster/sept2014JulMinTemp/sept2014JulMinTemp')
+# band = dataset.GetRasterBand(1)
+#
+# cols = dataset.RasterXSize
+# rows = dataset.RasterYSize
+#
+# t = dataset.GetGeoTransform()
+#
+# xOrigin_min7 = t[0]
+# yOrigin_min7 = t[3]
+# pixelWidth_min7 = t[1]
+# pixelHeight_min7 = -t[5]
+#
+# min7 = band.ReadAsArray(0, 0, cols, rows)
+
 # --------------------------------------------------------------------
 #      8. MAXTEMP JAN
 # --------------------------------------------------------------------
 
+# dataset = gdal.Open('../raster/sept2014JanMaxTemp/sept2014JanMaxTemp')
+# band = dataset.GetRasterBand(1)
+#
+# cols = dataset.RasterXSize
+# rows = dataset.RasterYSize
+#
+# t = dataset.GetGeoTransform()
+#
+# xOrigin_max1 = t[0]
+# yOrigin_max1 = t[3]
+# pixelWidth_max1 = t[1]
+# pixelHeight_max1 = -t[5]
+#
+# max1 = band.ReadAsArray(0, 0, cols, rows)
 # --------------------------------------------------------------------
-#      9. RADIOMETRICS 1
+#      9. RADIOMETRICS th
 # --------------------------------------------------------------------
 
+# dataset = gdal.Open('../raster/Radiometrics_2014_th/Radiometrics_2014_th')
+# band = dataset.GetRasterBand(1)
+#
+# cols = dataset.RasterXSize
+# rows = dataset.RasterYSize
+#
+# t = dataset.GetGeoTransform()
+#
+# xOrigin_radioth = t[0]
+# yOrigin_radioth = t[3]
+# pixelWidth_radioth = t[1]
+# pixelHeight_radioth = -t[5]
+#
+# radioth = band.ReadAsArray(0, 0, cols, rows)
+
 # --------------------------------------------------------------------
-#      10. RADIOMETRICS 2
+#      10. RADIOMETRICS k
 # --------------------------------------------------------------------
+
+# dataset = gdal.Open('../raster/Radiometrics_2014_k/Radiometrics_2014_k')
+# band = dataset.GetRasterBand(1)
+#
+# cols = dataset.RasterXSize
+# rows = dataset.RasterYSize
+#
+# t = dataset.GetGeoTransform()
+#
+# xOrigin_radiok = t[0]
+# yOrigin_radiok = t[3]
+# pixelWidth_radiok = t[1]
+# pixelHeight_radiok = -t[5]
+#
+# radiok = band.ReadAsArray(0, 0, cols, rows)
 
 # --------------------------------------------------------------------
 #      11. PROTECTION INDEX
 # --------------------------------------------------------------------
 
+# dataset = gdal.Open('../raster/ProtectionIndex/ProtectionIndex')
+# band = dataset.GetRasterBand(1)
+#
+# cols = dataset.RasterXSize
+# rows = dataset.RasterYSize
+#
+# t = dataset.GetGeoTransform()
+#
+# xOrigin_pi = t[0]
+# yOrigin_pi = t[3]
+# pixelWidth_pi = t[1]
+# pixelHeight_pi = -t[5]
+#
+# pi = band.ReadAsArray(0, 0, cols, rows)
+
 # --------------------------------------------------------------------
 #      12. VERTICAL DISTANCE
 # --------------------------------------------------------------------
+
+# dataset = gdal.Open('../raster/log_vertical_distance_saline_wetlands_sept2012/log_vertical_distance_saline_wetlands_sept2012')
+# band = dataset.GetRasterBand(1)
+#
+# cols = dataset.RasterXSize
+# rows = dataset.RasterYSize
+#
+# t = dataset.GetGeoTransform()
+#
+# xOrigin_vertical = t[0]
+# yOrigin_vertical = t[3]
+# pixelWidth_vertical = t[1]
+# pixelHeight_vertical = -t[5]
+#
+# vertical = band.ReadAsArray(0, 0, cols, rows)
 
 # --------------------------------------------------------------------
 #      13. LAND COVER
 # --------------------------------------------------------------------
 
+# dataset = gdal.Open('../raster/land_cov_use3/land_cov_use3')
+# band = dataset.GetRasterBand(1)
+#
+# cols = dataset.RasterXSize
+# rows = dataset.RasterYSize
+#
+# t = dataset.GetGeoTransform()
+#
+# xOrigin_land = t[0]
+# yOrigin_land = t[3]
+# pixelWidth_land = t[1]
+# pixelHeight_land = -t[5]
+#
+# land = band.ReadAsArray(0, 0, cols, rows)
+
 # --------------------------------------------------------------------
 #      14. IBRA HEX
 # --------------------------------------------------------------------
+
+# dataset = gdal.Open('../raster/ibra_hex/ibra_hex')
+# band = dataset.GetRasterBand(1)
+#
+# cols = dataset.RasterXSize
+# rows = dataset.RasterYSize
+#
+# t = dataset.GetGeoTransform()
+#
+# xOrigin_ibra = t[0]
+# yOrigin_ibra = t[3]
+# pixelWidth_ibra = t[1]
+# pixelHeight_ibra = -t[5]
+#
+# ibra = band.ReadAsArray(0, 0, cols, rows)
 
 # --------------------------------------------------------------------
 #      15. HYDRA
 # --------------------------------------------------------------------
 
+# dataset = gdal.Open('../raster/hydro500xwi/hydro500xwi')
+# band = dataset.GetRasterBand(1)
+#
+# cols = dataset.RasterXSize
+# rows = dataset.RasterYSize
+#
+# t = dataset.GetGeoTransform()
+#
+# xOrigin_hydra = t[0]
+# yOrigin_hydra = t[3]
+# pixelWidth_hydra = t[1]
+# pixelHeight_hydra = -t[5]
+#
+# hydra = band.ReadAsArray(0, 0, cols, rows)
+
 # --------------------------------------------------------------------
 #      16. ECO REGION 1
 # --------------------------------------------------------------------
+
+# dataset = gdal.Open('../raster/ecoregion1750/ecoregion1750')
+# band = dataset.GetRasterBand(1)
+#
+# cols = dataset.RasterXSize
+# rows = dataset.RasterYSize
+#
+# t = dataset.GetGeoTransform()
+#
+# xOrigin_eco1 = t[0]
+# yOrigin_eco1 = t[3]
+# pixelWidth_eco1 = t[1]
+# pixelHeight_eco1 = -t[5]
+#
+# eco1 = band.ReadAsArray(0, 0, cols, rows)
 
 # --------------------------------------------------------------------
 #      17. ECO REGION 2
 # --------------------------------------------------------------------
 
+# dataset = gdal.Open('../raster/ecoregion2014/ecoregion2014')
+# band = dataset.GetRasterBand(1)
+#
+# cols = dataset.RasterXSize
+# rows = dataset.RasterYSize
+#
+# t = dataset.GetGeoTransform()
+#
+# xOrigin_eco2 = t[0]
+# yOrigin_eco2 = t[3]
+# pixelWidth_eco2 = t[1]
+# pixelHeight_eco2 = -t[5]
+#
+# eco2 = band.ReadAsArray(0, 0, cols, rows)
+
 # --------------------------------------------------------------------
 #      18. HEATING
 # --------------------------------------------------------------------
+#
+# dataset = gdal.Open('../raster/Anisotrophic_Heating_Ruggedness/Anisotrophic_Heating_Ruggedness')
+# band = dataset.GetRasterBand(1)
+#
+# cols = dataset.RasterXSize
+# rows = dataset.RasterYSize
+#
+# t = dataset.GetGeoTransform()
+#
+# xOrigin_heat = t[0]
+# yOrigin_heat = t[3]
+# pixelWidth_heat = t[1]
+# pixelHeight_heat = -t[5]
+#
+# heat = band.ReadAsArray(0, 0, cols, rows)
+
 
 # --------------------------------------------------------------------
 #      19. STREAMS
+# --------------------------------------------------------------------
+
+# dataset = gdal.Open('../raster/75m_dem_streams_burned_sept2012/75m_dem_streams_burned_sept2012')
+# band = dataset.GetRasterBand(1)
+#
+# cols = dataset.RasterXSize
+# rows = dataset.RasterYSize
+#
+# t = dataset.GetGeoTransform()
+#
+# xOrigin_streams = t[0]
+# yOrigin_streams = t[3]
+# pixelWidth_streams = t[1]
+# pixelHeight_streams = -t[5]
+#
+# streams = band.ReadAsArray(0, 0, cols, rows)
+
 # --------------------------------------------------------------------
 
 input = pd.read_excel('input_observations.xlsx') # input data (observations)
@@ -177,8 +388,20 @@ species = input['COMMON_NME']
 lat = input['LATITUDEDD_NUM']
 long = input['LONGITUDEDD_NUM']
 
+# new excel sheet created
+workbook = xlsxwriter.Workbook('VBA_Raster.xlsx')
+worksheet = workbook.add_worksheet()
 
-#processRaster('./raster/wetness_index_saga_sept2012/wetness_index_saga_sept2012', point)
+worksheet.write('A1', 'TAXON_ID')
+worksheet.write('B1', 'COMMON_NME')
+worksheet.write('C1', 'RELIABILITY')
+worksheet.write('D1', 'RELIABILITY_TXT')
+worksheet.write('E1', 'LATITUDEDD_NUM')
+worksheet.write('F1', 'LONGITUDEDD_NUM')
+worksheet.write('G1', 'RECORD_TYPE')
+worksheet.write('H1', 'SV_RECORD_COUNT')
+
+workbook.close()
 
 print("SPECIES".ljust(25), " LAT".ljust(15), " LONG".ljust(10), " VEG".ljust(5),
       " WETNESS".ljust(5), " SUMMER".ljust(10), " RAINFALL".ljust(6))
@@ -187,11 +410,38 @@ for idx, val in enumerate(species):
     point = changeCoordinateSystem((long[idx], lat[idx]))
 
     print(species[idx].ljust(25), ' ', str(lat[idx]).ljust(10),' ', str(long[idx]).ljust(10), ' ' ,
-          str(calculateRaster(point, yOrigin, xOrigin, veg, pixelWidth, pixelHeight)).ljust(3), ' ',
-          str(calculateRaster(point, yOrigin_wet, xOrigin_wet, wet, pixelWidth_wet, pixelHeight_wet)).ljust(3), ' ',
+          # str(calculateRaster(point, yOrigin, xOrigin, veg, pixelWidth, pixelHeight)).ljust(3), ' ',
+          # str(calculateRaster(point, yOrigin_wet, xOrigin_wet, wet, pixelWidth_wet, pixelHeight_wet)).ljust(3), ' ',
           # str(calculateRaster(point, yOrigin_summer, xOrigin_summer, summer, pixelWidth_summer, pixelHeight_summer)).ljust(10), ' ',
+          # str(calculateRaster(point, yOrigin_summer75, xOrigin_summer75, summer75, pixelWidth_summer75, pixelHeight_summer75)).ljust(10), ' ',
           # str(calculateRaster(point, yOrigin_rain7, xOrigin_rain7, rain7, pixelWidth_rain7,
           #                     pixelHeight_rain7)).ljust(6), ' ',
           # str(calculateRaster(point, yOrigin_rain1, xOrigin_rain1, rain1, pixelWidth_rain1,
           #                     pixelHeight_rain1)).ljust(6), ' '
+          # str(calculateRaster(point, yOrigin_min7, xOrigin_min7, min7, pixelWidth_min7,
+          #                     pixelHeight_min7)).ljust(10), ' ',
+          # str(calculateRaster(point, yOrigin_max1, xOrigin_max1, max1, pixelWidth_max1,
+          #                     pixelHeight_max1)).ljust(10), ' ',
+          # str(calculateRaster(point, yOrigin_radioth, xOrigin_radioth, radioth, pixelWidth_radioth,
+          #                     pixelHeight_radioth)).ljust(10), ' ',
+          # str(calculateRaster(point, yOrigin_radiok, xOrigin_radiok, radiok, pixelWidth_radiok,
+          #                     pixelHeight_radiok)).ljust(10), ' ',
+          # str(calculateRaster(point, yOrigin_pi, xOrigin_pi, pi, pixelWidth_pi,
+          #                     pixelHeight_pi)).ljust(10), ' ',
+          # str(calculateRaster(point, yOrigin_vertical, xOrigin_vertical, vertical, pixelWidth_vertical,
+          #                     pixelHeight_vertical)).ljust(10), ' ',
+          # str(calculateRaster(point, yOrigin_land, xOrigin_land, land, pixelWidth_land,
+          #                     pixelHeight_land)).ljust(10), ' ',
+          # str(calculateRaster(point, yOrigin_ibra, xOrigin_ibra, ibra, pixelWidth_ibra,
+          #                     pixelHeight_ibra)).ljust(10), ' ',
+          # str(calculateRaster(point, yOrigin_hydra, xOrigin_hydra, hydra, pixelWidth_hydra,
+          #                     pixelHeight_hydra)).ljust(10), ' ',
+          # str(calculateRaster(point, yOrigin_eco1, xOrigin_eco1, eco1, pixelWidth_eco1,
+          #                     pixelHeight_eco1)).ljust(10), ' ',
+          # str(calculateRaster(point, yOrigin_eco2, xOrigin_eco2, eco2, pixelWidth_eco2,
+          #                     pixelHeight_eco2)).ljust(10), ' ',
+          # str(calculateRaster(point, yOrigin_heat, xOrigin_heat, heat, pixelWidth_heat,
+          #                     pixelHeight_heat)).ljust(10), ' ',
+          # str(calculateRaster(point, yOrigin_streams, xOrigin_streams, streams, pixelWidth_streams,
+          #                     pixelHeight_streams)).ljust(10), ' ',
           )
