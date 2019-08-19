b974372e2aca6e27e7c9c3645cebd36f931f2dcc
Jonathan Yeung <jonathanyeung1997@gmail.com>
2019-08-19 13:19:54 +1000

    Generated trees for all species

---
+++ generateAllTrees.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import numpy as np
+from sklearn.datasets.species_distributions import construct_grids
+import graphviz
+from sklearn import datasets, tree
+import matplotlib.pyplot as plt
+import geopy.distance
+from decisionTree import generateTree
+
+generateTree("Agile Antechinus")
+generateTree("Brown Treecreeper")
+generateTree("Common Beard-heath")
+generateTree("Small Triggerplant")
+generateTree("Southern Brown Tree Frog")
+generateTree("White-browed Treecreeper")
