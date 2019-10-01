e0f6a4836c0e5680745e9b760c2fd1629e5b6115
Gabby <salufie@gmail.com>
2019-10-01 21:02:11 +1000

    Generated models for all species and stored them
    We no longer need to regenerate models

--- main.py
+++ main.py
@@ -4,14 +4,21 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn import metrics
 from sklearn.model_selection import train_test_split
 from process_observation_data import writeToFile
+import pickle
 
-""" Main Menu: Get's user input and executes the selected option"""
+""" 
+Main Menu: Get's user input and executes the selected option. 
+The question the user will be asked is "has your observations data come pre-processed".
+
+"""
 def menu():
+    #generate_model('sample_output.xlsx', './combined_data/combined_white_browed_treecreeper.xlsx', "white_browed_treecreeper.pkl")
+
     print("# --------------------------------------------------------------------")
     print("#                      MAIN MENU                                      ")
     print("# --------------------------------------------------------------------")
     print("Note: this program can be run via command line by typing 'python3 main.py <observations>'. ")
-    print("Hello! Today we'll be processing a users observations results and displaying their reliability outcomes.")
+    print("\nHello! Today we'll be processing OBSERVATIONS and displaying their corresponding RELIABILITY outcomes.")
     print("Has your observations data come pre-processed? ")
     print("1. Yes")
     print("2. No")
@@ -22,7 +29,7 @@ def menu():
         file_name = input("File name: ")
 
         # note for the demo input: testing.xlsx
-        agileModel(file_name)
+        test(file_name)
 
     elif data_preprocessed == "2":
         print("Alright! Pre-processing the data now. NOTE: this may take a while. Please allow a few minutes.")
@@ -34,8 +41,39 @@ def menu():
         print("Sorry. Invalid input. Valid options are: 1, 2")
 
 
+def test(input_file):
+    input = pd.read_excel(input_file)
+
+    del input['TAXON_ID']
+    del input['COMMON_NME']
+    del input['CDE_TYPE']
+    del input['RECORD_TYPE']
+
+    input[input == np.inf] = np.nan
+    input.fillna(0, inplace=True)
+
+    clf = pickle.load(open("./models/agile_model.pkl", 'rb'))
+
+    print("Features importance", clf.feature_importances_)
+
+    print("\nPrinting reliability outcomes for the input file now...")
+
+    # print prediction
+    for i in range(len(input)):
+        predictions = clf.predict([input.loc[i, :]])
+        predicted_probs = clf.predict_proba([input.loc[i, :]])
+
+        if predicted_probs[0][0] > 0.70:
+            print("\nFor row ", i, ". The observation IS reliable")
+        else:
+            print("\nFor row ", i, ". The observation IS NOT reliable")
+
+        # print("\nFor row ", i, ". Overall reliability is: ", predictions)
+        print("Prediction percentages", predicted_probs[0])
+
+
 """ Sample Agile Antechinus model using the random foresting method"""
-def agileModel(input_file):
+def generate_model(input_file, training_file, pickle_name):
     # Input observations, run raster data through them
     # writeToFile('input_observations.xlsx', 'testing.xlsx')
 
@@ -45,7 +83,7 @@ def agileModel(input_file):
     input = pd.read_excel(input_file)
 
     # VBA data- MODEL INFORMATION
-    df = pd.read_excel('./combined_data/combined_agile_antechinus.xlsx')  # VBA (training data)
+    df = pd.read_excel(training_file)  # VBA (training data)
 
     # get rid of unneeded values (to improve accuracy of model)
     del df['COMMON_NME']
@@ -54,6 +92,7 @@ def agileModel(input_file):
     del df['RELIABILITY_TXT']
     del df['SV_RECORD_COUNT']
 
+    del input['TAXON_ID']
     del input['COMMON_NME']
     del input['CDE_TYPE']
     del input['RECORD_TYPE']
@@ -84,6 +123,8 @@ def agileModel(input_file):
 
     clf.fit(X_train, y_train)
     y_pred = clf.predict(X_test)
+
+    pickle.dump(clf, open(pickle_name, 'wb'))
 
     # print accuracy and feature importance
     print("Accuracy:", metrics.accuracy_score(y_test, y_pred))
