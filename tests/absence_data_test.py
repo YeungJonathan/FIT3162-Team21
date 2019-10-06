7b7531608bde9acce3fd464632bf06cc1fd78e04
Jonathan Yeung <jonathanyeung1997@gmail.com>
2019-10-06 22:42:13 +1100

    COmments

--- tests/absence_data_test.py
+++ tests/absence_data_test.py
@@ -8,8 +8,16 @@ import absenceData
 
 import unittest
 
+'''
+This test is used to test whether there is a balanced amount of pseudo absence data compared to VBA
+To run this test, run
+    python absence_data_test.py
+'''
 class TestPseudoAbsenceDataLength(unittest.TestCase):
 
+    '''
+    Test for Agile Antechinus
+    '''
     def test_absencedata_generation_length_agile_antechinus(self):
         absenceData.generateAbsenceData('Agile Antechinus', '../tests/testing_absence/agile_antechinus_test.xlsx');
         wb = openpyxl.load_workbook('testing_absence/agile_antechinus_test.xlsx')
@@ -22,6 +30,9 @@ class TestPseudoAbsenceDataLength(unittest.TestCase):
 
         self.assertGreaterEqual(numOfItem, row_count)
 
+    '''
+    Test for Brown_treecreeoer
+    '''
     def test_absencedata_generation_length_brown_treecreeper(self):
         absenceData.generateAbsenceData('Brown Treecreeper', '../tests/testing_absence/brown_treecreeper_test.xlsx');
         wb = openpyxl.load_workbook('testing_absence/brown_treecreeper_test.xlsx')
@@ -34,7 +45,10 @@ class TestPseudoAbsenceDataLength(unittest.TestCase):
 
         self.assertGreaterEqual(numOfItem, row_count)
 
-        
+    
+    '''
+    Test for Common Beard heath
+    '''
     def test_absencedata_generation_length_common_beard_heath(self):
         absenceData.generateAbsenceData('Common Beard-heath', '../tests/testing_absence/common_beard_heath_test.xlsx');
         wb = openpyxl.load_workbook('testing_absence/common_beard_heath_test.xlsx')
@@ -48,6 +62,9 @@ class TestPseudoAbsenceDataLength(unittest.TestCase):
         self.assertGreaterEqual(row_count, numOfItem)
 
         
+    '''
+    Test for small triggerplant 
+    '''
     def test_absencedata_generation_length_small_triggerplant_test(self):
         absenceData.generateAbsenceData('Small Triggerplant', '../tests/testing_absence/small_triggerplant_test.xlsx');
         wb = openpyxl.load_workbook('testing_absence/small_triggerplant_test.xlsx')
@@ -60,7 +77,10 @@ class TestPseudoAbsenceDataLength(unittest.TestCase):
 
         self.assertGreaterEqual(row_count, numOfItem)
 
-        
+    
+    '''
+    Test for tree frog
+    '''
     def test_absencedata_generation_length_tree_frog_test(self):
         absenceData.generateAbsenceData('Southern Brown Tree Frog', '../tests/testing_absence/tree_frog_test.xlsx');
         wb = openpyxl.load_workbook('testing_absence/tree_frog_test.xlsx')
@@ -73,7 +93,9 @@ class TestPseudoAbsenceDataLength(unittest.TestCase):
 
         self.assertGreaterEqual(row_count, numOfItem)
 
-        
+    '''
+    Test for white browed treecreeper
+    '''
     def test_absencedata_generation_length_white_browed_treecreeper_test(self):
         absenceData.generateAbsenceData('White-browed Treecreeper', '../tests/testing_absence/white_browed_treecreeper_test.xlsx');
         wb = openpyxl.load_workbook('testing_absence/white_browed_treecreeper_test.xlsx')
